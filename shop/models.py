bf847b36137bdec2e22a9ab64ade9022ef43d63a
Kusov Alexandr <kusov.alex2004@mail.ru>
2023-04-05 15:59:03 +0300

    Add Categories

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -11,6 +12,10 @@ class Category(models.Model):
         ordering = ('name', ) #упорядочевание категории по имени
         verbose_name = 'category' #для правильного написания моделей во множественном числе
         verbose_name_plural = 'categories'
+
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug])
+
     def __str__(self):
         return self.name
 
