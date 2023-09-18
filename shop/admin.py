41a64a120c103c97a9b2c5bd693a2366480dea1a
Alexandr <kusov.alex2004@mail.ru>
2023-09-18 12:10:47 +0300

    Расширил БД

--- shop/admin.py
+++ shop/admin.py
@@ -3,5 +3,11 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Category)
+admin.site.register(Manufacturer)
 admin.site.register(Product)
 admin.site.register(Users)
+admin.site.register(Discount_For_Product_Category)
+admin.site.register(Product_Images)
+admin.site.register(Comments)
+admin.site.register(Emails)
+admin.site.register(Characteristic)
--- shop/models.py
+++ shop/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
 from autoslug import AutoSlugField
+from sortedm2m.fields import SortedManyToManyField
 
 
 # Create your models here.
@@ -19,6 +20,18 @@ class Users(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Characteristic(models.Model):
+    characteristic_name = models.CharField(max_length=30, verbose_name='Название характеристки')
+    value = models.CharField(max_length=20, verbose_name='Значение')
+
+    def __str__(self):
+        return f"{self.characteristic_name} {self.value}"
+
+    class Meta:
+        verbose_name = 'Характеристика товара'
+        verbose_name_plural = 'Характеристи товаров'
 
 
 class Category(models.Model):
@@ -43,13 +56,15 @@ class Product(models.Model):
     name = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
     description = models.TextField(blank=True)
+    manufacturer = models.ForeignKey('Manufacturer', on_delete=models.PROTECT, null=True, verbose_name='Производитель')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=0)
-    image = models.ImageField(upload_to='product', blank=True)
     stock = models.IntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
+    product_photos = SortedManyToManyField('Product_Images', verbose_name='Изображения')
+    product_characteristic = SortedManyToManyField(Characteristic, verbose_name='Характеристики')
 
     class Meta:
         ordering = ('name',)  # упорядочевание категории по имени
@@ -89,3 +104,69 @@ class CartItem(models.Model):
 
     def __str__(self):
         return self.product
+
+
+class Manufacturer(models.Model):
+    manufacturer_name = models.CharField(max_length=100, verbose_name="Наименование производителя", unique=True)
+    country = models.CharField(max_length=40, verbose_name='Страна', blank=True)
+    slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name='URL', )
+    photo = models.ImageField(upload_to='img_category/%Y/%m/%d/', verbose_name='Фотография',
+                              default='shop/static/img/favicon.svg')
+
+    def __str__(self):
+        return self.manufacturer_name
+
+    class Meta:
+        verbose_name = 'Производитель'
+        verbose_name_plural = 'Производители'
+
+
+class Discount_For_Product_Category(models.Model):
+    category_name = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория товаров',
+                                      blank=True)
+    discount_percentage = models.FloatField(verbose_name='Скидка')
+    discount_start_date = models.DateField(verbose_name='Начало скидок', auto_now_add=True)
+    discount_end_date = models.DateField(verbose_name='Конец скидок')
+    slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name='URL', )
+
+    class Meta:
+        verbose_name = 'Скидка на категорию продукта'
+        verbose_name_plural = 'Скидки на категории продуктов'
+
+
+class Product_Images(models.Model):
+    img_name = models.CharField(max_length=50, verbose_name='Картинка изображения', blank=True)
+    img = models.ImageField(upload_to='img_product/%Y/%m/%d/', verbose_name='Изображение продукта')
+    first_img = models.BooleanField(default=False, verbose_name='Главная картинка')
+    slug = models.SlugField(max_length=50, unique=True, db_index=True, verbose_name='URL')
+
+    def __str__(self):
+        return self.img_name
+
+    class Meta:
+        verbose_name = 'Фотография продукта'
+        verbose_name_plural = 'Фотографии продуктов'
+
+
+class Comments(models.Model):
+    user = models.ForeignKey('Users', on_delete=models.CASCADE, verbose_name="Пользователь")
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, verbose_name="Продукты", related_name='comments')
+    text = models.TextField(verbose_name="Комментарий", blank=True)
+    rating = models.IntegerField(verbose_name='Оценка от 1 до 10 ')
+    date = models.DateField(verbose_name='Время', auto_now_add=True, blank=True)
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+        ordering = ['rating']
+
+
+class Emails(models.Model):
+    email = models.EmailField(verbose_name='Почта')
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Неоправленное письмо'
+        verbose_name_plural = 'Неоправленные письма'
